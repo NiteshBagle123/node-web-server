993ffd270a382ee486bcb48db315266349b3216a
Bagle <baglen@pun-baglen-m.ds.ad.adp.com>
2020-06-28 23:19:20 +0530

    handlers bar implementation

--- src/app.js
+++ src/app.js
@@ -1,9 +1,34 @@
+const hbs = require('hbs');
 const path = require('path');
 const express = require('express');
 const app = express();
 
+// define paths for express config
 const publicDirectoryFile = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// setup handlers engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// setup static directory to serve
 app.use(express.static(publicDirectoryFile));
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Nitesh Bagle'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About App',
+        name: 'Nitesh Bagle'
+    });
+});
 
 app.get('/weather', (req, res) => {
     res.send({
@@ -12,8 +37,27 @@ app.get('/weather', (req, res) => {
     });
 });
 
-app.get('*', (req, res)=> {
-    res.send('Page not found...');
+app.get('/help', (req, res) => {
+    res.render('help',{
+        title: 'Help App',
+        name: 'Nitesh Bagle'
+    });
+});
+
+app.get('/help/*', (req, res)=> {
+    res.render('404', {
+        title: 'Help page',
+        errorMessage: 'Help page not found',
+        name: 'Nitesh Bagle'
+    });
+});
+
+app.all('*', (req, res)=> {
+    res.render('404', {
+        title: 'Generic Page',
+        errorMessage: 'Page not found',
+        name: 'Nitesh Bagle'
+    });
 });
 
 app.listen(3000, () => {
